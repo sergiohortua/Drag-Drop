fa99bd94e3737e4a6a5e4abf6f92149ff4310c01
Sergio Hortua <sergioandresjsv@gmail.com>
2021-05-13 16:25:19 -0500

    feature-estructura en Modal para DnD

---
+++ src/Components/ClassBoard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ClassBoard extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href="#">De'onde se cojen los Counter</a>
+        <span>*******************************************************</span>
+        <a href="#">Pa'onde van los Counters</a>
+      </div>
+    );
+  }
+}
+export default ClassBoard;
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import '../style/Modal.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "../style/Modal.css";
+import ClassBoard from "./ClassBoard";
 
-
-const Modal =(props)=>{   
-if(!props.isOpen){
-    return null
-}
-        return(
-            ReactDOM.createPortal(
-            <div className="Modal">
-            <div className="Modal__container">
-              <button onClick={props.onClose} className="Modal__close-button">
-                X
-              </button>
-                soy el modal del portal
-              {props.children}
-            </div>
-          </div>,document.getElementById('modal'))
-        )
-}
-export default Modal
+const Modal = (props) => {
+  if (!props.isOpen) {
+    return null;
+  }
+  return ReactDOM.createPortal(
+    <div className="Modal">
+      <div className="Modal__container">
+        <button onClick={props.onClose} className="Modal__close-button">
+          X
+        </button>
+        <ClassBoard />
+      </div>
+    </div>,
+    document.getElementById("modal")
+  );
+};
+export default Modal;
