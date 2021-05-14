a7bba8f7710326db28dc28b8c46c2a80372f478e
Sergio Hortua <sergioandresjsv@gmail.com>
2021-05-13 21:18:41 -0500

    feature-Drag&Drop funcional v1

--- src/Components/ClassBoard.jsx
+++ src/Components/ClassBoard.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import Card from "./Card";
+import Board from "./Board";
+import '../style/ClassBoard.css'
 
-class ClassBoard extends React.Component {
-  render() {
-    return (
-      <div>
-        <a href="#">De'onde se cojen los Counter</a>
-        <span>*******************************************************</span>
-        <a href="#">Pa'onde van los Counters</a>
-      </div>
-    );
-  }
+export default function ClassBoard(props) {
+  return (
+    <div className="container">
+      <Board id="board-1" className="board">
+        <Card id="card-1" className="card" draggable="true">
+          <p>Card 1111 provider</p>
+        </Card>
+      </Board>
+      <Board id="board-2" className="board">
+        <Card id="card-2" className="card" draggable="true">
+          <p>Card 2222 provider</p>
+        </Card>
+      </Board>
+    </div>
+  );
 }
-export default ClassBoard;
+
